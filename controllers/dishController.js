2ad29846564128026069dd9b6e4e0c948820fcff
TYLPHE <tylphet@gmail.com>
2023-02-06 23:43:52 -0700

    dish form and recipe form complete. need to work on restaurant form

--- controllers/dishController.js
+++ controllers/dishController.js
@@ -1,6 +1,7 @@
 const dish = require('../models/dish');
 const async = require('async');
 const recipe = require('../models/recipe');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all dishes
 exports.dish_list = (req, res, next) => {
@@ -21,34 +22,71 @@ exports.dish_list = (req, res, next) => {
 
 // Display detail page for a specific dish
 exports.dish_detail = (req, res, next) => {
-  dish.findById(req.params.id, function(err, dishDetail) {
+  dish.findById(req.params.id, async function(err, dishDetail) {
     if(err) {
       return next(err);
     }
-    console.log('dishdetail', dishDetail)
-    recipe.findById(dishDetail.recipe, function(err, recipeDetail) {
-      if(err) {
-        return next(err);
-      }
-      console.log(recipeDetail)
-      res.render('dish_detail', {
-        title: 'Dish detail',
-        dish: dishDetail,
-        recipe: recipeDetail
-      });
+    // recipeArr is an array of objects to find recipe names to display on detail page
+    const recipeArr = await recipe.find({ '_id': { $in: dishDetail.recipe } });
+
+    res.render('dish_detail', {
+      title: 'Dish detail',
+      dish: dishDetail,
+      recipe: recipeArr,
     });
   });
 };
 
 // Display dish create form GET
 exports.dish_create_get = (req, res) => {
-  res.send('not implemented');
+  res.render('dish_form', { title: 'Create Dish' });
 }
 
 // Display dish create on POST
-exports.dish_create_post = (req, res) => {
-  res.send('not implemented');
-}
+exports.dish_create_post = [
+  // Validate and sanitize the form
+  body('dishName', 'Dish name required').trim().isLength({min:1}).escape(),
+  body('price', 'Must be a positive integer').isInt({min:0}),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Create dish object with escaped and trimmed data
+    const dishObj = new dish(
+      {
+        dishName: req.body.dishName.charAt(0).toUpperCase() + req.body.dishName.slice(1),
+        price: req.body.price,
+      }
+    );
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages
+      res.render('dish_form', {
+        title: 'Create dish',
+        dishObj,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data form is valid
+      // Check if dish with the same name already exists
+      dish.findOne({ dishName: req.body.dishName }).exec((err, found_dish) => {
+        if (found_dish) {
+          res.redirect(found_dish.url);
+        } else {
+          dishObj.save((err) => {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(dishObj.url);
+          });
+        }
+      });
+    }
+  }
+];
 
 // Display dish delete form on GET
 exports.dish_delete_get = (req, res) => {
